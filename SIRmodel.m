0fee927ba660e8d457a0683d85d99102112672be
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2020-03-18 18:43:57 +0100

    SIR model tests

--- SIRmodel.m
+++ SIRmodel.m
@@ -8,32 +8,41 @@
 
 nPop=5441120;                   % Population of Slovakia
 infected=108;                   % Current number of infected
-days=60;                        % Days to run the simulation
-resolution=1/24;                % Integration resolution is hours
+days=300;                       % Days to run the simulation
+resolution=24;                  % Integration resolution is hours
+
+R0=1.5;                           % Reproduction factor
+recovery=7;                    % Average recovery time
 
 x0 = [nPop-infected; infected; 0];  % Initial state: Susceptible, Infected, Recovered
 t = linspace(0, days, days*resolution);         % Time vector, duration, data
-[t y] = ode45(@SIR, t, x0);         % Model integration using ode45
+[t x] = SIR(t, x0, R0, recovery)
 
 figure(1)
-plot(t, [y]);
-legend ('Susceptible', 'Infectious', 'Recovered');
+plot(t, [x,x(:,2)+x(:,3)]);
+legend ('Susceptible', 'Infectious', 'Recovered','Infected');
 grid on
-title('SIR Simulation')
+title('SARS-CoV-2 SIR Simulation for Slovakia')
 xlabel('Time (Days)')
-ylabel('Numer of people')
+ylabel('Numer of cases')
 % figure(2)
 % plot(diff(y(:,3)))
 % legend('Daily cases')
 
-function xdot = SIR(t, x)
+
+function [t x] = SIR(t, x0, R0, recovery)
+
+[t x] = ode45(@SIRbase,t,x0);
+
+function xdot = SIRbase(t, x);
 xdot = zeros(3, 1);
 
-R0=7; 
-gamma = 0.0714; % recovery rate 1/g = e.g. 14 [days]
+gamma = 1/recovery; % recovery rate 1/g = e.g. 14 [days]
 beta=R0*gamma;% probability of contracting the disease (1.4-2.5)
-N = x(1) + x(2) + x(3); % a fixed population for 500 individuals
-xdot(1) = - beta*x(2)*x(1)/N; % Susceptible differential equation
-xdot(2) = beta*x(2)*x(1)/N - gamma*x(2); % Infectious differential equation
-xdot(3) = gamma*x(2); % Recovered differential equation
+
+N = x(1) + x(2) + x(3);                  % Computing total population from original data
+xdot(1) = - beta*x(2)*x(1)/N;            % Differential equation for Susceptible cases
+xdot(2) =   beta*x(2)*x(1)/N - gamma*x(2); % Differential equation for Infected cases
+xdot(3) =   gamma*x(2);                     % Differential equation for Recovered cases
+end
 end
